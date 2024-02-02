6952a80a23305ed546ca8b11c70c7a91d16c0be5
Vedant Desai <40174946+vedant1803@users.noreply.github.com>
2024-02-02 16:15:00 +0530

    commit

---
+++ client/src/components/Item/Item.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Item.css";
+
+const Item = ({ image, name, new_price }) => {
+  return (
+    <div className="item">
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Item;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="logo_img" />
         <p>SHOPPERS STOP</p>
       </div>
-      <ul>
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <NavLink to="/">Shop{menu === "shop" && <hr />}</NavLink>
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          <NavLink to="/mens">Men{menu === "men" && <hr />}</NavLink>
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          <NavLink to="/womens">Women{menu === "women" && <hr />}</NavLink>
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <NavLink to="/kids">Kids{menu === "kids" && <hr />}</NavLink>
+        </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="cart_icon" />
+        <NavLink
+          to="/login"
+          onClick={() => {
+            setMenu("");
+          }}
+        >
+          <button>Login</button>
+        </NavLink>
+        <NavLink
+          to="/cart"
+          onClick={() => {
+            setMenu("");
+          }}
+        >
+          <img src={cart_icon} alt="cart_icon" />
+        </NavLink>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
